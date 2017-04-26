2369f24c9e47353d8d8373e49b4f714ede26bb0b
Matthew Palmer <matthewpalmer@Matthews-MacBook-Pro.local>
2017-04-26 19:42:59 +0100

    photoswipe plugin adding for gallery

--- includes/footer.php
+++ includes/footer.php
@@ -3,42 +3,12 @@
 
         <div class="footer">
 
-
-            jgiajrl
-
-
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
         <script src="scripts/jquery.js"></script>
         <script type="text/javascript" src="scripts/bootstrap.min.js"></script>
         <script src="scripts/navigation-controls.js"></script>
-        <script src="scripts/velocity.js"></script>
-        <script src="scripts/muuri.js"></script>
-        <script>
-        var grid = new Muuri({
-            container: document.getElementsByClassName('grid')[0],
-            // Muuri does not convert a node list to array automatically
-            // so we have to do it manually.
-            items: [].slice.call(document.getElementsByClassName('item'))
-        });
-
-        </script>
     
     </body>
 
--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,22 @@
         <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="styles/bootstrap.min.css">
         <link rel="stylesheet" href="styles/style.css">
+        
+        <!-- Core CSS file -->
+        <link rel="stylesheet" href="styles/photoswipe.css"> 
+
+        <!-- Skin CSS file (styling of UI - buttons, caption, etc.)
+             In the folder of skin CSS file there are also:
+             - .png and .svg icons sprite, 
+             - preloader.gif (for browsers that do not support CSS animations) -->
+        <link rel="stylesheet" href="styles/default-skin/default-skin.css"> 
+
+        <!-- Core JS file -->
+        <script src="scripts/photoswipe.min.js"></script> 
+
+        <!-- UI JS file -->
+        <script src="scripts/photoswipe-ui-default.min.js"></script> 
+        <script src="scripts/jquery.js"></script>
     </head>
 
     <body>
